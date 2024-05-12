c72c920a9912e4343c2f73f21bd22370da87a4b0
Rob Green <robertma.green@gmail.com>
2024-05-12 21:03:58 +0100

    our light within

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'FRACTALS';
+export const SITE_TITLE = 'OUR LIGHT WITHIN';
 export const SITE_DESCRIPTION = 'Expertly Curated Diamonds';
